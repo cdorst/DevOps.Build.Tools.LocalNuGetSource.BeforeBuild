f62073efe44130df4976c690508d0280f0bae32d
Christopher Dorst <chris@dorst.email>
2018-03-04 00:44:50 -0600

    Add "dotnet before.dll" tool to upload NuGet packages to blob storage

---
+++ DevOps.Build.Tools.LocalNuGetSource.BeforeBuild/PackageDictionaryHelper.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace DevOps.Build.Tools.LocalNuGetSource.BeforeBuild
+{
+    internal static class PackageDictionaryHelper
+    {
+        public static void Add(this Dictionary<string, string> dictionary, string line)
+            => dictionary.TryAdd(
+                line.Parse("Include"),
+                line.Parse("Version"));
+
+        public static string GetFileName(this KeyValuePair<string, string> pair)
+            => $"{pair.Key}.{pair.Value}.nupkg";
+
+        private static string Parse(this string line, string token)
+            => line.Split($"{token}=\"").Last().Split("\"").First();
+    }
+}
--- DevOps.Build.Tools.LocalNuGetSource.BeforeBuild/Program.cs
+++ DevOps.Build.Tools.LocalNuGetSource.BeforeBuild/Program.cs
@@ -1,12 +1,53 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using static Common.Functions.DownloadAndExtractZip.ZipDownloaderAndExtractor;
 
 namespace DevOps.Build.Tools.LocalNuGetSource.BeforeBuild
 {
-    class Program
+    public static class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var token = args.FirstOrDefault() ?? throw new ArgumentNullException("NamespacePrefix");
+            var cacheDirectory = args[1] ?? throw new ArgumentNullException("CacheDirectory");
+            var packageUri = args[2] ?? throw new ArgumentNullException("PackageUri");
+
+            // Get list of required NuGet packages
+            var packages = new Dictionary<string, string>();
+
+            Console.WriteLine("Finding .csproj...");
+            var dir = Environment.GetEnvironmentVariable("APPVEYOR_BUILD_FOLDER");
+            var files = Directory.EnumerateFiles(dir, " *.csproj", SearchOption.AllDirectories);
+            foreach (var file in files)
+            {
+                Console.WriteLine($"Found .csproj: {file}");
+                Console.WriteLine($"Getting {token} package references...");
+                var lines = File.ReadAllLines(file);
+                foreach (var line in lines.Where(ln
+                    => ln.Contains("PackageReference") && ln.Contains($"\"{token}.")))
+                    packages.Add(line);
+            }
+
+            // Save each package in local NuGet cache
+            foreach (var package in packages)
+            {
+                var name = package.GetFileName();
+                Console.WriteLine($"Looking for package: {name}...");
+                if (File.Exists(Path.Combine(cacheDirectory, name))) continue;
+
+                try
+                {
+                    Console.WriteLine($"Caching package: {name}...");
+                    DownloadAndExtract(new Uri($"{packageUri}/{name}.zip"), cacheDirectory);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Error: {ex.Message}...");
+                    // Ignore 404 or network exception and continue
+                }
+            }
         }
     }
 }
